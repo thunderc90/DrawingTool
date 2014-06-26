a32cf067409e84db00332da5253f547713401ec2
Zach Buckley <buckley.zach@gmail.com>
2014-06-25 21:53:45 -0400

    new updates that were never committed to the repo

---
+++ src/buckley/drawingtool/enums/DeleteType.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buckley.DrawingTool.enums;
+
+/**
+ *
+ * @author Thunder
+ */
+public enum DeleteType {
+    DELETE_ALL,
+    DELETE_TOP
+}
